ebf5e01850950d3aaa11b59a96eda2767e574c1e
Peter Harpending <peter@harpending.org>
2016-03-05 11:47:08 -0700

    Add a skeleton Haskell file.

---
+++ skel/Skel.hs
@@ -0,0 +1,30 @@
+-- Snowdrift.coop - cooperative funding platform
+-- Copyright (c) 2012-2016, Snowdrift.coop
+-- 
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+-- 
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+-- details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- | 
+-- Module      : Skel
+-- Description : Short description
+-- Copyright   : Copyright (c) 2012-2016, Snowdrift.coop.
+-- License     : AGPL-3
+-- Maintainer  : dev@lists.snowdrift.coop
+-- Stability   : experimental
+-- Portability : POSIX
+-- 
+-- Here is a longer description of this module, containing some
+-- commentary with @some markup@.
+
+module Skel where
+
